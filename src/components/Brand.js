f4435c2e061c1b14c7cdef86f4aac3351411a721
Dorian Guilmain <guilmain.dorian@gmail.com>
2020-02-21 11:43:16 +0100

    :sparkles: Add Brand Component

---
+++ src/components/Brand.js
@@ -0,0 +1,33 @@
+import { setPragma, styled } from "goober";
+import { h } from "preact";
+
+setPragma(h);
+
+const Wrapper = styled("div")(({ isCollapsed }) => ({
+  display: "flex",
+  alignItems: isCollapsed ? "end" : "center",
+  justifyContent: "center",
+  width: '100%',
+  height: '80px',
+  overflow: "hidden"
+}));
+
+const Logo = styled("img")({
+});
+
+const CollapsedLogo = styled("img")({
+});
+
+const Brand = ({ logo, collapsedLogo, isCollapsed }) => {
+  return (
+    <Wrapper isCollapsed={isCollapsed}>
+      {isCollapsed ? (
+        <CollapsedLogo src={collapsedLogo} />
+      ) : (
+        <Logo src={logo} />
+      )}
+    </Wrapper>
+  );
+};
+
+export default Brand;
