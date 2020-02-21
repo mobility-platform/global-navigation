6ff3279745687afc52e1033b3ed8aff3bdb4e88f
Dorian Guilmain <guilmain.dorian@gmail.com>
2020-02-21 17:56:04 +0100

    :lipstick: Rework spacing

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -8,7 +8,7 @@ const Wrapper = styled("div")({
   alignItems: "center",
   width: "100%",
   height: "40px",
-  padding: "10px",
+  padding: "10px 20px",
   boxSizing: "border-box",
   borderRadius: "50%"
 });
