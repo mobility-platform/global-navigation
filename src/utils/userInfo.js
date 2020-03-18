9698e09ef8bfb890e18e8368294acae071067f4a
Guilmain Dorian <guilmain.dorian@gmail.com>
2020-03-18 10:21:43 +0100

    :recycle: Clean userInfo display logic

--- src/utils/userInfo.js
+++ src/utils/userInfo.js
@@ -1,30 +1,7 @@
-import MD5 from "./MD5";
-
 export const displayName = userInfo => {
-  if (userInfo.name) {
-    return userInfo.name;
-  }
-  if (userInfo.email) {
-    return userInfo.email;
-  }
-  if (userInfo.phone_number) {
-    return userInfo.phone_number;
-  }
-  return `Unknown`;
+  return userInfo.name;
 };
 
 export const displayPicture = userInfo => {
-  if (userInfo.picture) {
-    return userInfo.picture;
-  }
-  if (userInfo.name) {
-    return `http://www.gravatar.com/avatar/${MD5(userInfo.name)}.jpg?s=182`;
-  }
-  if (userInfo.email) {
-    return `http://www.gravatar.com/avatar/${MD5(userInfo.email)}.jpg?s=182`;
-  }
-  if (userInfo.phone_number) {
-    return `http://www.gravatar.com/avatar/${MD5(userInfo.phone_number)}.jpg?s=182`;
-  }
-  return `https://www.gravatar.com/avatar/`;
+  return userInfo.picture;
 };
