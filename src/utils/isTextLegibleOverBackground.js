70441b13b8bbe345172fab9fdf37cd2a98ea9f03
Antoine Colmard <a.colmard@gmail.com>
2020-04-01 10:35:40 +0200

    :pencil2: Rename luminance functions

--- src/utils/isTextLegibleOverBackground.js
+++ src/utils/isTextLegibleOverBackground.js
@@ -1,23 +1,27 @@
-function getLFromRgbValue(value) {
-  var normalizedValue = value / 255;
+function getLuminanceFromRgbValue(value) {
+  const normalizedValue = value / 255;
   return 0.03928 > normalizedValue
     ? normalizedValue / 12.92
     : Math.pow((normalizedValue + 0.055) / 1.055, 2.4);
 }
 
-function getLFromRgbColor(r, g, b) {
-  return 0.2126 * getLFromRgbValue(r) + 0.7152 * getLFromRgbValue(g) + 0.0722 * getLFromRgbValue(b);
+function getLuminanceFromRgbColor(r, g, b) {
+  return (
+    0.2126 * getLuminanceFromRgbValue(r) +
+    0.7152 * getLuminanceFromRgbValue(g) +
+    0.0722 * getLuminanceFromRgbValue(b)
+  );
 }
 
-function getLFromHex(hex) {
-  var [, r, g, b] = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return getLFromRgbColor(parseInt(r, 16), parseInt(g, 16), parseInt(b, 16));
+function getLuminanceFromHex(hex) {
+  const [, r, g, b] = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return getLuminanceFromRgbColor(parseInt(r, 16), parseInt(g, 16), parseInt(b, 16));
 }
 
 export default function isTextLegibleOverBackground(textColor, backgroundColor) {
-  var textLightness = getLFromHex(textColor),
-    backgroundLightness = getLFromHex(backgroundColor);
-  var contrast =
+  const textLightness = getLuminanceFromHex(textColor),
+    backgroundLightness = getLuminanceFromHex(backgroundColor);
+  const contrast =
     (Math.max(textLightness, backgroundLightness) + 0.05) /
     (Math.min(textLightness, backgroundLightness) + 0.05);
   return contrast >= 3;
