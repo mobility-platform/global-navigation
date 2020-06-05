512a29d04221231ca90d87d3c467eeea32ffc3e6
Guilmain Dorian <guilmain.dorian@gmail.com>
2020-06-05 15:17:23 +0200

    :bento: Add Bell icon

--- src/utils/SVG.js
+++ src/utils/SVG.js
@@ -75,6 +75,24 @@ export const FiGrid = () => {
   );
 };
 
+export const FiBell = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+    >
+      <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+      <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+    </svg>
+  );
+};
+
 export const FiArrowLeft = () => {
   return (
     <svg
